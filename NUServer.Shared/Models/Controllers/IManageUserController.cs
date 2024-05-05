915bb3e393e90d9be69a0ddf7b9b46f76e7a9ad7
Timur Meyliev <timka654work@gmail.com>
2024-05-06 02:42:43 +0300

    wip

---
+++ NUServer.Shared/Models/Controllers/IManageUserController.cs
@@ -0,0 +1,20 @@
+﻿#if SERVER
+using Microsoft.AspNetCore.Mvc;
+#endif
+
+using NSL.Generators.HttpEndPointGenerator.Shared.Attributes;
+using NSL.HttpClient.Models;
+using NUServer.Shared.Models.Request;
+using NUServer.Shared.Models.Response;
+
+namespace NUServer.Shared.Models.Controllers
+{
+    [HttpEndPointContainerGenerate("api/manage/[controller]")]
+    public interface IManageUserController
+    {
+
+        [HttpEndPointGenerate(typeof(DataResponse<SignResponseModel>))] Task<IActionResult> SignIn([FromBody] SignInRequestModel query);
+
+        [HttpEndPointGenerate(typeof(DataResponse<SignResponseModel>))] Task<IActionResult> SignUp([FromBody] SignUpRequestModel query);
+    }
+}
